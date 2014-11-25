abb536782d30c274747175ec7eb02385f49a5907
Justin Veirs <justinveirs@gmail.com>
2014-11-25 15:48:18 -0500

    trying to get js to work

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	});
-})
+	.mouseleave(function) {
+		// alert('mouse left');
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	});
+	.mousedown(function) {
+		console.log('mousedown');
+		//play sound
+		//show hadouken
+	});
+	.mouseup(function) {
+		console.log('mouseup');
+	});
+});
