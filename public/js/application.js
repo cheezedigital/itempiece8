7eaba8e3665f7548ace52e554628a61b9c2fd1a1
Justin Veirs <justinveirs@gmail.com>
2014-11-26 18:39:12 -0500

    add file gitignore to clean up messy DS_Store shit

--- public/js/application.js
+++ public/js/application.js
@@ -2,46 +2,53 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	});
+	})
 
-	$('.ryu').mouseleave(function() {
+	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-	});
+	})
 
-	$('.ryu').mousedown(function() {
+	.mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
+		$('.ryu-still').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show()
 		.animate(
 			{'left': '300px'},
 			500,
 			function() {
-				$('this').hide();
-				$('this').css('left', '-212px');
+				$(this).hide();
+				$(this).css('left', '-212px');
 			}
 		);
-	});
+	})
 
-	$('.ryu').mouseup(function() {
+	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
+		});
 	});
 
-	$(document).on('keydown', function(e) {
-		if (e.keycode == 88)
-			$('.ryu-ready').hide();
-			$('ryu-cool').ready();
-		};
-	});
+	$(document).keydown(function(e) {
+		 	if (e.keyCode == 88) {
 
+				$('.ryu-still').hide();
+				$('.ryu-ready').hide();
+				$('ryu-cool').show();
+		  }
+		}).keyup(function(e) {
+			if (e.keyCode == 88) {
+
+				$('ryu-cool').hide();
+				$('ryu-still').show();
+			}
+	});
 
 	function playHadouken() {
 		$('#hadouken-sound')[0].volume = 0.5;
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
 	}
-
-
 });
