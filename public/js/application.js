0d1648ecfc96907f41ed79c166efbb5f00328583
Justin Veirs <justinveirs@gmail.com>
2014-11-26 17:26:36 -0500

    sound not working

--- public/js/application.js
+++ public/js/application.js
@@ -1,24 +1,37 @@
 $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
-		alert('mouse entered .ryu-div');
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	});
 
 	$('.ryu').mouseleave(function() {
-		alert('mouse left');
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	});
 
 	$('.ryu').mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$('this').hide();
+				$('this').css('left', '-212px');
+			}
+		);
 	});
 
 	$('.ryu').mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+
+	function playHadouken() {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
 });
