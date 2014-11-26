fb38a68cad6c327b67c54baa97d6e0b240d04853
Justin Veirs <justinveirs@gmail.com>
2014-11-26 18:58:53 -0500

    adding new sound and cleaning up js

--- public/js/application.js
+++ public/js/application.js
@@ -36,13 +36,13 @@ $(document).ready(function() {
 
 				$('.ryu-still').hide();
 				$('.ryu-ready').hide();
-				$('ryu-cool').show();
+				$('.ryu-cool').show();
 		  }
 		}).keyup(function(e) {
 			if (e.keyCode == 88) {
 
-				$('ryu-cool').hide();
-				$('ryu-still').show();
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
 			}
 	});
 
@@ -50,5 +50,5 @@ $(document).ready(function() {
 		$('#hadouken-sound')[0].volume = 0.5;
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
-	}
+	};
 });
